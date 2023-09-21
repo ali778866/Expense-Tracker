4714a98591114d4f29c09dc9d0819a59c4e60965
ali778866 <mdali.ali76@gmail.com>
2023-09-21 22:34:40 +0530

    leaderboard optimisation done

--- controller/leaderboard.js
+++ controller/leaderboard.js
@@ -1,27 +1,22 @@
 const Expense = require('../model/expense');
 const User = require('../model/user');
+const sequelize = require('../utility/database');
 
 exports.usertotalexp = async (req, res, next) => {
     try{
-        const users = await User.findAll()  
-        const expenses = await Expense.findAll();
-        const userAggregatedExp = {};
-        expenses.forEach(expense => {
-            const { userId, amount } = expense;
-            // console.log(typeof(amount));
-            if(userAggregatedExp[userId]){
-                userAggregatedExp[userId] += parseInt(amount);
-            } else {
-                userAggregatedExp[userId] = parseInt(amount);
-            }   
-        });
-        var userLeaderboardDetails = [];
-        users.forEach((user) => {
-             userLeaderboardDetails.push({ name: user.name, totalExpense: userAggregatedExp[user.id] || 0})
-        })
-        userLeaderboardDetails.sort((a,b) =>  b.totalExpense - a.totalExpense);
-        console.log(userLeaderboardDetails);
-        res.status(200).json(userLeaderboardDetails);
+        const leaderboardexp = await User.findAll({
+            attributes: ['id', 'name', [sequelize.fn('sum' , sequelize.col('expenses.amount')), 'totalExpense']],
+            include: [
+                {
+                    model: Expense,
+                    attributes:[]
+                }
+            ],
+            group: ['user.id']
+        })  
+        console.log(leaderboardexp);
+        res.status(200).json(leaderboardexp);
+
         } catch(err){
             console.error("Error adding user:", err);
             res.status(500).json({
--- model/expense.js
+++ model/expense.js
@@ -9,7 +9,7 @@ const Expense = sequelize.define('expense', {
         allowNull: false,
         primaryKey: true
     },
-    amount: Sequelize.STRING,
+    amount: Sequelize.INTEGER,
     description: Sequelize.STRING,
     category: Sequelize.STRING
 })
